09ae7f882e5f532a2e3a434c3d4234fbc5237472
sumanya-ece <2501020017@mail.jiit.ac.in>
2025-12-24 16:45:07 +0530

    Update tempchangebychoice.cpp

--- basic-math/tempchangebychoice.cpp
+++ basic-math/tempchangebychoice.cpp
@@ -1,3 +1,4 @@
+// Program to convert temperature using user choice
 #include <iostream>
 using namespace std;
 int main(){
@@ -7,19 +8,20 @@ int main(){
     cout<<"Enter 2 to convert temp to celsius "<<endl;
     cout<<"Enter choice: ";
     cin>>choice;
-
+    //Choice 1: Celsius to Fahrenheit
     if(choice==1){
     cout<<"Enter the temperature in celsius: ";
     cin>>temp;
         cout<<"Temperature in fahrenheit is: "<<(temp * 9 / 5) + 32<<endl;
     }
+    //Choice 2: Fahrenheit to Celsius
     else if(choice==2){
         cout<<"Enter the temperature in fahrenheit: ";
     cin>>temp;
         cout<<"Temperature in celsius is: "<<(temp - 32) * 5 / 9<<endl;
     }
     else
-        cout<<"Invalid choice";
+        cout<<"Invalid choice"<<endl;
     return 0;
 
 
